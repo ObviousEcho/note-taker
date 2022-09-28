81a1177ad70d36b288df0aa4d56cb5a5ea40b7e1
Adia & Indy <107782632+ObviousEcho@users.noreply.github.com>
2022-09-28 00:20:40 -0600

    created get route for api/notes

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,3 +1,17 @@
 const notes = require('express').Router();
+const fs = require('fs');
+
+notes.get('/', (req, res) => {
+    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+        // err ? console.error(err) : console.log(data);
+        if(err) {
+            console.error(err);
+        } else {
+            console.log(data);
+            res.json(data);
+        }
+    })
+    
+})
 
 module.exports = notes;
--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-const api = require("./routes/index");
+const api = require("./routes/index.js");
 
 const PORT = process.env.PORT || 3001;
 
@@ -11,10 +11,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api", api);
 
+// GET route for homepage, serves up a static html file
 app.use(express.static("public"));
 
+// GET route for notes page
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "public/notes.html"))
 );
+
+// Wildcard route to direct users to a 404 page
+// app.get("*", (req, res) =>
+//   res.sendFile(path.join(__dirname, "/public/404.html"))
+// );
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
