1ee7608298a09173ae41118f3424bea144c11dea
Adia & Indy <107782632+ObviousEcho@users.noreply.github.com>
2022-09-29 22:30:03 -0600

    created post method in routes/notes.js

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,17 +1,45 @@
-const notes = require('express').Router();
-const fs = require('fs');
+const notes = require("express").Router();
+const fs = require("fs");
 
-notes.get('/', (req, res) => {
-    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
-        // err ? console.error(err) : console.log(data);
-        if(err) {
-            console.error(err);
-        } else {
-            console.log(data);
-            res.json(data);
-        }
-    })
-    
-})
+notes.get("/", (req, res) => {
+  fs.readFile("./db/db.json", "utf-8", (err, data) => {
+    err ? console.error(err) : res.json(JSON.parse(data));
+  });
+});
+
+notes.post("/", (req, res) => {
+  console.info(`${req.method} request received to add a note`);
+  console.log(req.body);
+  const { title, text } = req.body;
+  const newNote = {
+    title,
+    text,
+  };
+
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedNotes = JSON.parse(data);
+
+      parsedNotes.push(newNote);
+
+      fs.writeFile(
+        "./db/db.json",
+        JSON.stringify(parsedNotes, null, 4),
+        (writeErr) =>
+          writeErr
+            ? console.error(writeErr)
+            : console.info("Successfully added note!")
+      );
+    }
+  });
+  const response = {
+    status: "success",
+    body: newNote,
+  };
+  console.log(response);
+  res.status(201).json(response);
+});
 
 module.exports = notes;
