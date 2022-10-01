4f880923dcb1d80887ae05c275203298bd917d43
Adia & Indy <107782632+ObviousEcho@users.noreply.github.com>
2022-09-30 20:58:24 -0600

    added npm uuid, wrote delete method in notes.js, hitting endpoint though api required editing

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,4 +1,5 @@
 const notes = require("express").Router();
+const { v4: uuidv4 } = require("uuid");
 const fs = require("fs");
 
 notes.get("/", (req, res) => {
@@ -14,6 +15,7 @@ notes.post("/", (req, res) => {
   const newNote = {
     title,
     text,
+    id: uuidv4(),
   };
 
   fs.readFile("./db/db.json", "utf8", (err, data) => {
@@ -40,6 +42,28 @@ notes.post("/", (req, res) => {
   };
   console.log(response);
   res.status(201).json(response);
+});
+
+notes.delete("/:id", (req, res) => {
+  const noteId = req.params.id;
+  fs.readFile("./db/db.json", "utf-8", (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedNotes = JSON.parse(data);
+      const result = parsedNotes.filter((note) => note.note_id !== noteId);
+      console.log(result);
+
+      fs.writeFile(
+        "./db/db.json",
+        JSON.stringify(result, null, 4),
+        (writeErr) =>
+          writeErr
+            ? console.error(writeErr)
+            : res.json(`Note ${noteId} has been deleted 🗑️`)
+      );
+    }
+  });
 });
 
 module.exports = notes;
